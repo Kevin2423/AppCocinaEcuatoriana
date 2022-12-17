23c124ff97049e55dc581989643c640d9ea6285f
DavidLargo423 <90362166+Kevin2423@users.noreply.github.com>
2022-12-17 14:20:26 -0500

    Segunda Subida

--- Pantallas/Receta2.js
+++ Pantallas/Receta2.js
@@ -1,43 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Botonmenu } from "../Componentes/Botonmenu";
-import { Tituloing } from '../Componentes/Tituloing';
-import { Titulopre } from '../Componentes/Titulopre';
-import { Titulo } from '../Componentes/Titulo';
+import { Tituloing } from "../Componentes/Tituloing";
+import { Titulopre } from "../Componentes/Titulopre";
+import { Titulo } from "../Componentes/Titulo";
 export default function Receta1({ navigation }) {
   return (
     <View style={styles.container}>
-      <Titulo texttitulo="Chifles"></Titulo>
-      <Image style={styles.imagendis}
-        source={require("../src/imagen/Receta2.jpg")} />
+      <Titulo texttitulo="Ensalada de Vegetales"></Titulo>
+      <Image
+        style={styles.imagendis}
+        source={require("../src/imagen/Receta2.jpg")}
+      />
       <Tituloing />
-      <Text>{`
-Dos platanos
-Aceite
-Sal`}
+      <Text>
+        {`
+2 zanahorias, Sal, pimienta
+1 maduro (plátano verde maduro) para adornar
+1/4 kg (1/2 b) de guisantes
+1/2 kg (1 lb) de judías verdes
+1 kg (2 lb) de patatas
+2 tomates, Aceite`}
       </Text>
       <Titulopre />
-      <Text>{`
-1. Corte los plátanos en rodajas muy finas.
-2. Fríalos en abundante aceite muy caliente hasta que estén dorados.
-3. Escúrralos muy bien sobre papel absorbente y espolvoréelos con
-sal fina.
-4. Sírvalos muy calientes como aperitivo. `}
+      <Text>
+        {`
+PREPARACIÓN PARA 4 PERSONAS
+1.- Cueza las zanahorias, las judías y las patatas
+en abundante agua hirviendo.
+2.- Cuando esté todo tierno, escurra las verduras y
+déjelas enfriar.
+3.- Corte las patatas y las zanahorias en dados,
+los tomates en rodajas y las judías verdes en
+trozos pequeños.
+4.-Mezcle todas las verduras y alíñelas con aceite.
+Salpimiente al gusto.
+5.-Decore la ensalada con maduros.`}
       </Text>
       <StatusBar style="auto" />
-      <Botonmenu
-        text="Menu"
-        onPress={() => navigation.navigate('Menu')}
-      />
+      <Botonmenu text="Menu" onPress={() => navigation.navigate("Menu")} />
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   imagendis: {
     width: 400,
